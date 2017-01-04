2b4115905fbc702737ee0cd652fd58086da65ea9
MrYone <yonebluzs@gmail.com>
2017-01-04 17:06:11 +0800

    ,

--- PB/C++/C++Primer/test.c
+++ PB/C++/C++Primer/test.c
@@ -1,19 +1,72 @@
-#include "stdio.h"
-#define MAXSIZE 100
-int main (){
-	char a[MAXSIZE];
+#include <curses.h>
+#include "time.h"
+#include "stdlib.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-	printf("enter a string: ");
+int mygetch( ) {
+	struct termios oldt,
+				   newt;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldt );
+	newt = oldt;
+	newt.c_lflag &= ~( ICANON | ECHO );
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+	return ch;
+}
+int kbhit(void)
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+		   
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+					   
+	ch = getchar();
+						 
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+							 
+	if(ch != EOF){
+		ungetc(ch, stdin);
+		return 1;
+	}
+							   
+	return 0;
+}
 
-	scanf("%s",a);
+char UserEnter(){
+	int time = clock();
+	int c;
+	int white = 10000000;
 
-	printf("\n\t");
+	printf("i white you %d sec to enter something\n",white);
+	while(1){
+		if(kbhit())
+			return mygetch();
+		if(clock() - time > white)
+			return '\0';
+	}
+}
 
-	for(int i = 0; a[i] != '\0';i++)
-		printf("%c",a[i] + 1);
 
-	printf("\n");
-	
+int main (){	
+	char c;
+	if(c = UserEnter())
+		printf("\nUser enter key is : %c\n",c);
+	else
+		printf("\nUser do not enter something!\n");
 	return 0;
 }
 
