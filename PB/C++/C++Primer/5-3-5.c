a5939ec5b32f1ad4b928da6c329fa52c8a7d6e50
MrYone <yonebluzs@gmail.com>
2017-01-13 16:58:37 +0800

    .

--- PB/C++/C++Primer/5-3-5.c
+++
@@ -1,8 +0,0 @@
-#include "time.h"
-#include "stdlib.h"
-#include <stdio.h>
-
-int main(){
-
-	return 0;
-}
---
+++ PB/C++/C++Primer/作业/7.3.1.cpp
@@ -0,0 +1,13 @@
+#include "string.h"
+#include "iostream"
+
+using namespace std;
+
+int main(){
+	string UserEnter;
+	char * Enter;
+	streamsize n = 100;
+	cin >> UserEnter ;
+	cout << UserEnter << endl;
+	return 0;
+}
