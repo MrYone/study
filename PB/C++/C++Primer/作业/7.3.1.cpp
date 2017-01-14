d7989a456c763ec28e532950d37db227fc8cc7ad
MrYone <yonebluzs@gmail.com>
2017-01-14 09:55:42 +0800

    .

--- PB/C++/C++Primer/作业/7.3.1.cpp
+++ PB/C++/C++Primer/作业/7.3.1.cpp
@@ -1,13 +1,20 @@
-#include "string.h"
 #include "iostream"
 
 using namespace std;
 
+const int MAX = 100;
 int main(){
-	string UserEnter;
-	char * Enter;
-	streamsize n = 100;
-	cin >> UserEnter ;
-	cout << UserEnter << endl;
+	char UserEnter[MAX];
+	//int Length;
+
+	cin.getline(UserEnter,MAX);
+	cout << strlen(UserEnter) << endl;
+	for(int i = strlen(UserEnter)-1;i >= 0 ; i --){
+		if((UserEnter[i]>='a')&&(UserEnter[i]<='z')||(UserEnter[i]>='A')&&(UserEnter[i]<='Z'))
+			cout << UserEnter[i];
+		//cout << endl;
+	}
+	cout << endl;
+	//cout << UserEnter << endl;
 	return 0;
 }
