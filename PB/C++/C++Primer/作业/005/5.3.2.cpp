c0ed31268a30ccc2ffdde44c7348068b9817bd54
MrYone <yonebluzs@gmail.com>
2017-01-16 17:03:07 +0800

    .

--- PB/C++/C++Primer/作业/005/5.3.2.cpp
+++ PB/C++/C++Primer/作业/005/5.3.2.cpp
@@ -7,7 +7,28 @@ int main(){
 	int *p0 = &n0,
 		*p1 = &n1,
 		*p2 = &n2;
-	
+
+	int temp ;
+
+	cin >> *p0;
+	cin >> *p1;
+	cin >> *p2;
+
+	for(int i = 3; i > 0; i--){
+		if(*p0 > *p1){
+			temp = *p0;
+			*p0 = *p1;
+			*p1 = temp;
+		}
+		if(*p1 > *p2){
+			temp = *p1;
+			*p1 = *p2;
+			*p2 = temp;
+		}
+	}
+
+	cout << n0 << "<" << n1 << "<" << n2 << endl;
+
 
 	return 0;
 }
