63885539e57b04705fbb3fa00befbbda53d309f7
MrYone <yonebluzs@gmail.com>
2017-01-21 23:34:38 +0800

    .

--- PB/C++/C++Primer/作业/006/6.3.3.cpp
+++ PB/C++/C++Primer/作业/006/6.3.3.cpp
@@ -1,8 +1,29 @@
 #include "iostream"
+#include "stdio.h"
 
 using namespace std;
+int max(int a , int b){
+	return (a > b ? a : b);
+}
+
+int min(int a , int b){
+	return (a > b ? b : a);
+}
+
+int sum(int a , int b){
+	return a + b;
+}
+
+int fun(int a , int b , int (*call)(int , int)){
+//int fun(int a , int b , auto (*call)(int ,int) -> int){ //不能这么写
+	return (call(a,b));
+}
 
 int main(){
-	 
+
+	printf("max = %d\n",fun(1,2,max));
+	printf("min = %d\n",fun(3,4,min));
+	printf("sum = %d\n",fun(5,6,sum));
+
 	return 0;
 }
--- PB/C++/C++Primer/作业/006/6.3.4.cpp
+++ PB/C++/C++Primer/作业/006/6.3.4.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+using uint = unsigned int;
+
+typedef struct BookInfo{
+	char *BookName;
+	char *Author;
+	int Price;	
+}BookInfo,*pBookInfo;
+
+bool initBook(pBookInfo &pBook,uint nCount){
+	pBook = new BookInfo[nCount];
+
+
+	for(int i = 0; i < nCount; i++){
+
+		pBook[i].BookName; 	
+	}
+	return true;	
+}
+
+
 int main(){
-	 
+	pBookInfo pBook = NULL;
+	initBook(pBook,10);
+
+
+
+	
 	return 0;
 }
